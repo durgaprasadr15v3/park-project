a90c9b29832142c4c9f0a910249b60967d438ff9
durgaprasadr15v3 <durgaprasadr15v3@gmail.com>
2024-02-27 21:05:28 +0530

    this my third update

--- src/main/java/com/example/parkproject/service/ParkService.java
+++ src/main/java/com/example/parkproject/service/ParkService.java
@@ -68,25 +68,49 @@ public class ParkService {
 		
 
 		List<UserRegistration> getall=userRepository.findAll();
-		int fir = 0,so = 0,t = 0;//,fo,fi,six,sev=0;
+		int fir = 0,so = 0,t = 0,fo=0,fi=0,six=0,sev=0,ei=0;
 		for(UserRegistration onebyone:getall)
 		{
 			if(onebyone.getAge()>=0&&onebyone.getAge()<=10)
 			{
 				fir++;
-				divideages.put("Zero to ten years childers are",fir);
+				divideages.put("Zero to ten years age childers count are",fir);
 			}
 			else if(onebyone.getAge()>=11&&onebyone.getAge()<=20)
 			{
 				so++;
-				divideages.put("eleven to twenty years boy&girls are ",so);
+				divideages.put("eleven to twenty years age boy&girls count are ",so);
 			}
 			else if(onebyone.getAge()>=21&&onebyone.getAge()<=30)
 			{
 				t++;
-				divideages.put("twentyone to thiry years people are",t);
+				divideages.put("twentyone to thiry years age people count are",t++);
 			}
-			
+			else if(onebyone.getAge()>=31&&onebyone.getAge()<=40)
+			{
+				divideages.put("thirtyone to fourty years age people count are", fo++);
+			}
+			else if(onebyone.getAge()>=41&&onebyone.getAge()<=50)
+			{
+				divideages.put("fourtyone to fifty  years age people count are", fi++);
+			}
+			else if(onebyone.getAge()>=51&&onebyone.getAge()<=60)
+			{
+				divideages.put("fiftyone to sixty years age people count are", six++);
+			}
+			else if(onebyone.getAge()>=61&&onebyone.getAge()<=70)
+			{
+				divideages.put("sixtyone to seventy years age people count are", sev++);
+			}
+			else 
+			{
+				ei++;
+				divideages.put("seventyone to and above age  people count are", ei);
+			}
+
+
+
+
 		}
 		
 		return divideages;
